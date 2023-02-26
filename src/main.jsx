8249e087c0e2794176c6a24b5c4d6d40e4818acf
sohseyedi <sohseyedi@gmail.com>
2023-02-26 11:16:27 +0330

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-import 'bootstrap/dist/css/bootstrap.min.css'
 import {Toaster} from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
